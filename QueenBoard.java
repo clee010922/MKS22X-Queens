6ddc1cc7b5efa9a298094e0a37bd5f414854d8f7
clee010922 <clee01@stuy.edu>
2019-02-07 09:14:58 -0500

    progress in class

--- QueenBoard.java
+++ QueenBoard.java
@@ -3,11 +3,11 @@ public class QueenBoard {
   private int[][] board;
 
   public QueenBoard(int size) {
-
+    board = new int[size][size];
   }
 
   private boolean addQueen(int r, int c) {
-
+    
   }
 
   private boolean removeQueen(int r, int c) {
